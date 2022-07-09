8c6107f2d1e7ab8daf59f0a0e6eab8e2cfa03490
Alexander Hogle <alexanderhogle@gmail.com>
2022-07-09 14:28:30 -0500

    some placeholder pokemon data

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,13 +1,49 @@
 import React from 'react';
-import { Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import * as Yup from 'yup';
 
+import colors from '../config/colors';
 import Screen from '../components/Screen';
 import { Form, FormField, SubmitButton } from '../components/forms';
 
 const validationSchema = Yup.object().shape({
   search: Yup.string().required().label('Enter Pokemon Name')
 });
+
+const pokemon = [
+  {
+    order: 120,
+    name: 'Staryu',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/yellow/120.png',
+    type: {
+      name: 'Normal'
+    },
+    flavor_text_entries: [
+      {
+        flavor_text:
+          'An enigmatic\nPOKéMON that can\neffortlessly\fregenerate any\nappendage it\nloses in battle.'
+      }
+    ],
+    weight: 30,
+    height: 10
+  },
+  {
+    order: 1,
+    name: 'Bulbasaur',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/yellow/120.png',
+    type: {
+      name: 'grass'
+    },
+    flavor_text_entries: [
+      {
+        flavor_text:
+          'An enigmatic\nPOKéMON that can\neffortlessly\fregenerate any\nappendage it\nloses in battle.'
+      }
+    ]
+  }
+];
 
 function HomeScreen(props) {
   return (
@@ -26,11 +62,27 @@ function HomeScreen(props) {
           autoCorrect={false}
           icon="pokeball"
           name="search"
-          placeholder="Enter Pokemon Name"
+          placeholder="Enter Pokémon Name"
           textContentType="name"
         />
         <SubmitButton title="Search" />
       </Form>
+      <View>
+        <Image
+          style={styles.pokemon}
+          source={{
+            uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/120.png'
+          }}
+        />
+        <Text>
+          {pokemon[0].name} #{pokemon[0].order}
+        </Text>
+
+        <Text>{pokemon[0].type.name}</Text>
+        <Text>
+          {pokemon[0].height} dm {pokemon[0].weight} lbs
+        </Text>
+      </View>
     </Screen>
   );
 }
@@ -46,6 +98,15 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginTop: 50,
     marginBottom: 40
+  },
+  pokemon: {
+    width: 100,
+    height: 100,
+    alignSelf: 'center',
+    marginTop: 50,
+    marginBottom: 40,
+    borderColor: colors.secondary,
+    borderWidth: 4
   }
 });
 
