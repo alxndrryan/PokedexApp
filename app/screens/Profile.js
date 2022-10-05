f720e7a84762a81916ae36e62c85946174e11eb7
Alexander Hogle <alexanderhogle@gmail.com>
2022-10-05 16:24:58 -0500

    building pokemon grid

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -4,62 +4,88 @@ import { Image, FlatList, StyleSheet, View } from 'react-native';
 import Screen from '../components/Screen';
 import colors from '../config/colors';
 
-const pokemon = [
+const itemData = [
   {
-    order: 120,
-    name: 'Staryu',
-    image:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/yellow/120.png'
+    icon: (
+      <Image
+        alt="staryu"
+        style={{ width: 100, height: 100 }}
+        source={{
+          uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/120.png'
+        }}
+      />
+    )
   },
   {
-    order: 1,
-    name: 'Bulbasaur',
-    image:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/yellow/120.png'
+    icon: (
+      <Image
+        alt="bulbasaur"
+        style={{ width: 100, height: 100 }}
+        source={{
+          uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/1.png'
+        }}
+      />
+    )
+  },
+  {
+    icon: (
+      <Image
+        alt="pikachu"
+        style={{ width: 100, height: 100 }}
+        source={{
+          uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/25.png'
+        }}
+      />
+    )
+  },
+  {
+    icon: (
+      <Image
+        alt="charmeleon"
+        style={{ width: 100, height: 100 }}
+        source={{
+          uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/5.png'
+        }}
+      />
+    )
+  },
+  {
+    icon: (
+      <Image
+        alt="onix"
+        style={{ width: 100, height: 100 }}
+        source={{
+          uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/95.png'
+        }}
+      />
+    )
+  },
+  {
+    icon: (
+      <Image
+        alt="jigglypuff"
+        style={{ width: 100, height: 100 }}
+        source={{
+          uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/39.png'
+        }}
+      />
+    )
   }
 ];
+
+const Item = ({ item }) => {
+  return <View style={styles.item}>{item.icon}</View>;
+};
 
 function Profile(props) {
   return (
     <Screen style={styles.container}>
-      <View style={styles.pokemonGrid}>
-        <Image
-          style={styles.pokemonImg}
-          source={{
-            uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/120.png'
-          }}
-        />
-        <Image
-          style={styles.pokemonImg}
-          source={{
-            uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/1.png'
-          }}
-        />
-        <Image
-          style={styles.pokemonImg}
-          source={{
-            uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/95.png'
-          }}
-        />
-        <Image
-          style={styles.pokemonImg}
-          source={{
-            uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/25.png'
-          }}
-        />
-        <Image
-          style={styles.pokemonImg}
-          source={{
-            uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/5.png'
-          }}
-        />
-        <Image
-          style={styles.pokemonImg}
-          source={{
-            uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/150.png'
-          }}
-        />
-      </View>
+      <FlatList
+        data={itemData}
+        numColumns={2}
+        renderItem={Item}
+        keyExtractor={(item) => item.source}
+      />
     </Screen>
   );
 }
@@ -67,10 +93,14 @@ function Profile(props) {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.primary,
-    padding: 10
+    flex: 2,
+    marginHorizontal: 'auto',
+    width: 'auto'
   },
-  pokemonGrid: {
-    display: 'grid'
+  item: {
+    flex: 1,
+    maxWidth: '50%',
+    alignItems: 'center'
   },
   pokemonImg: {
     width: 100,
